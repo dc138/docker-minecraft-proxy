9c7b94bc61bbc1d23cfefa052af8616af8e45289
DarthChungo <antonio@adaro.eu>
2023-06-19 00:18:11 +0200

    Add bungeecord support

--- entry.sh
+++ entry.sh
@@ -3,17 +3,17 @@
 #
 # Docker Minecraft Proxy, a simple docker image to run a minecraft proxy.
 # Copyright © 2023 Antonio de Haro
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -27,9 +27,14 @@
 get_version_info() {
   case $1 in
     "bungeecord")
-      url="none"
-      version="none"
-      ;;
+      if [ "$2" = "latest" ]; then
+        version=$(wget -q "https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/buildNumber" -O-)
+
+      elif $(echo $2 | grep -Eq "[0-9]+"); then
+        version=$2
+      fi
+
+      url="https://ci.md-5.net/job/BungeeCord/$version/artifact/bootstrap/target/BungeeCord.jar";;
 
     "waterfall")
       if [ "$2" = "latest" ]; then
